d163a846e03388e9cfe8e4085341e3353f94d236
Ubuntu <ubuntu@ip-172-31-32-102.ap-southeast-1.compute.internal>
2020-08-18 05:19:06 +0000

    Commit for day2 lab1

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    String welcomeMsg = "";
+
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        welcomeMsg = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        System.out.println(welcomeMsg);
+        return welcomeMsg;
     }
 }
